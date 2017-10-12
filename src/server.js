da448b676c625c68dc2d3e514406b3d009440ee8
ssghajar <ssghajar@gmail.com>
2017-10-12 23:46:06 +0330

    express server

---
+++ src/server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+let app = express()
+
+function run() {
+    app.get('/', function (req, res) {
+        console.log("someone connected")
+        res.send('Hello World')
+    })
+
+    app.get('/hello/', function (req, res) {
+        res.send('Hello tapir')
+    })
+
+    app.listen(80)
+}
+
+exports.run = run
